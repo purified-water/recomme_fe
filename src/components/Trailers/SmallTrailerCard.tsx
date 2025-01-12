79198e2e3c50840c7589314fc461b69cc121f91a
“purified-water” <julianng1110@gmail.com>
2025-01-13 02:13:07 +0700

    feat: add get lastest trailers

--- src/components/Trailers/SmallTrailerCard.tsx
+++ src/components/Trailers/SmallTrailerCard.tsx
@@ -1,4 +1,6 @@
 import { TrailerType } from "@/types/TrailerType";
+import { normalizeImagePath } from "@/utils/NormalizeMovieImage";
+import UNAVAILABLE_IMAGE from "@/assets/unavailable_image.jpg";
 
 interface SmallTrailerCardProps {
   trailer: TrailerType;
@@ -6,7 +8,7 @@ interface SmallTrailerCardProps {
 
 export const SmallTrailerCard = ({ trailer }: SmallTrailerCardProps) => {
   const handleTrailerClick = () => {
-    console.log(trailer.title);
+    console.log("Trailer clicked:", trailer.title);
   };
 
   return (
@@ -14,7 +16,11 @@ export const SmallTrailerCard = ({ trailer }: SmallTrailerCardProps) => {
       className="relative h-48 overflow-hidden transition-all duration-300 transform rounded-lg bg-gray6 w-80 group hover:scale-105 hover:rounded-lg hover:z-10 shrink-0"
       onClick={handleTrailerClick}
     >
-      <img src={trailer.image} alt={trailer.title} className="object-cover w-full h-full" />
+      <img
+        src={normalizeImagePath(trailer.backdrop_path) || UNAVAILABLE_IMAGE}
+        alt={trailer.title}
+        className="object-cover w-full h-full"
+      />
       <div className="absolute inset-0 flex items-center justify-center transition-opacity bg-black bg-opacity-50 opacity-0 group-hover:opacity-100">
         <button className="z-30 flex items-center justify-center w-12 h-12 text-black bg-white rounded-full shadow-lg hover:bg-gray-200">
           ▶
@@ -22,7 +28,7 @@ export const SmallTrailerCard = ({ trailer }: SmallTrailerCardProps) => {
       </div>
       <div className="absolute bottom-0 left-0 right-0 p-4 text-white bg-gradient-to-t from-black to-transparent">
         <h3 className="text-lg font-bold">{trailer.title}</h3>
-        <p className="text-sm text-gray-300">{trailer.description}</p>
+        <p className="text-sm text-gray-300 line-clamp-1">{trailer.overview}</p>
       </div>
     </div>
   );
